80bcc4493c374d1b9aeac4589985f55460c12cb5
astyer22 <astyer@byui.edu>
2024-04-23 21:14:07 -0600

    Prep one is completed

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Runtime.Versioning;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name?");
+        string fname = Console.ReadLine();
+
+        Console.Write("What is your last name?");
+        string lname = Console.ReadLine();
+
+        Console.Write($"Your name is {fname}, {fname} {lname}");
     }
 }
