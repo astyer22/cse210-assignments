dbd36a8ed0ef7f6da3aa35274c065867072a8dd3
astyer22 <astyer@byui.edu>
2024-04-23 22:32:06 -0600

    prep 3 completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenertor = new Random();
+        int magicNumber = randomGenertor.Next(1, 101);
+
+        while (true)
+        {
+            Console.Write("Guess the magic number: ");
+            string guess = Console.ReadLine();
+
+            if (magicNumber == int.Parse(guess))
+            {
+                Console.WriteLine("Good job! You guessed the magic number!");
+                break;
+            }
+            else if (magicNumber > int.Parse(guess))
+            {
+                Console.WriteLine("Too low! Try again!");
+            }
+            else if (magicNumber < int.Parse(guess))
+            {
+                Console.WriteLine("Too high! Try again!");
+            }
+        }
     }
 }
+
+      
