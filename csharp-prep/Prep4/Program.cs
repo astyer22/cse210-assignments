316828e13c3d2ee6d7b386f0bb8e8a8c5076281f
astyer22 <astyer@byui.edu>
2024-04-25 10:04:49 -0600

    prep 4 is completed

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,34 @@
 using System;
 
+using System.Collections.Generic;
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> Numbers = new List<int>();
+
+        Console.WriteLine("Enter a number, if you want to stop enter 0: ");
+
+        while (true)
+        {
+            Console.Write("Enter a number:" + " ");
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+
+            if (number != 0)
+            {
+                Numbers.Add(number);
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        Console.WriteLine($"The numbers you entered are: {string.Join(", ", Numbers)}");
+        Console.WriteLine($"The sum of the numbers is: {Numbers.Sum()}");
+        Console.WriteLine($"The maximum number is: {Numbers.Max()}");
+        Console.WriteLine($"The average of the numbers is: {Numbers.Average()}");
     }
 }
