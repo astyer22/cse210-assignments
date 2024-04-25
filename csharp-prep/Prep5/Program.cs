b0db944b54c7aeac8fb648a33ef98fe06f366711
astyer22 <astyer@byui.edu>
2024-04-25 17:49:28 -0600

    Prep 5 is completed

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcomeMessage("Welcome to the Program!");
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int square = SquareNumber(number, name);
+    }
+    
+    static void DisplayWelcomeMessage(string message)
+    {
+        Console.WriteLine(message);
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Enter a number: ");
+        string input = Console.ReadLine();
+        int number = int.Parse(input);
+        return number;
+    }
+
+    static int SquareNumber(int number, string name)
+    {
+        Console.Write($"Hello, {name}!" + " ");
+        Console.WriteLine($"The square of {number} is: {number * number}");
+        return number * number;
     }
 }
